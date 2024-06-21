18badca2ad776be0e993c600a5ae1d82471bb5c2
GabR3924 <garc2004@gmail.com>
2024-06-21 11:06:09 -0400

    plnas

--- src/data.js
+++ src/data.js
@@ -1,71 +1,46 @@
- const cards = [
-    {
+const cards = [
+  {
+      title: "Plan Básico",
+      precio: "10",
+      data: {
+          uno: "Daños a bienes (Según Ley)",
+          dos: "Daños a personas (Según Ley)",
+          tres: "Cobertura adicional (250,00)",
+          cuatro: "Defensa legal (250,00)",
+          cinco: "Cobertura por muerte accidental (Variable según ocupantes)",
+          seis: "Cobertura por invalidez permanente (Variable según ocupantes)",
+          siete: "Gastos médicos (Variable según ocupantes)",
+      },
 
-      title: "Plan Precavido ",
-      precio: "10", // Asigna la imagen importada aquí
+  },
+  {
+      title: "Plan Intermedio",
+      precio: "20",
       data: {
-        uno: "Daños a cosas (Segun Ley)",
-        dos: "Daños a Personas (Segun Ley)",
-        tres: "Exceso de Limite (250,00)",
-        cuatro: "Defensa Penal (250,00)",
-        cinco: "Muerte Accideltal (Dependiendo de la cantidad de puestos)",
-        seis: "Invalidez Permanente (Dependiendo de la cantidad de puestos)",
-        siete: "Gastos Medicos (Dependiendo de la cantidad de puestos)",
+          uno: "Daños a bienes (Según Ley)",
+          dos: "Daños a personas (Según Ley)",
+          tres: "Cobertura adicional (500 / 800)",
+          cuatro: "Defensa legal (500 / 800)",
+          cinco: "Cobertura por muerte accidental (Variable según ocupantes)",
+          seis: "Cobertura por invalidez permanente (Variable según ocupantes)",
+          siete: "Gastos médicos (Variable según ocupantes)",
       },
-      asegurado:{
-        uno: "Segun Ley",
-        dos: "Segun Ley",
-        tres: "250,00",
-        cuatro: "250,00",
-        cinco: "Dependiendo de la cantidad de puestos",
-        seis: "Dependiendo de la cantidad de puestos",
-        siete: "Dependiendo de la cantidad de puestos",
-      }
-    },
-    {
-      title: "Plan Prudente ",
-      precio: "20", // Asigna la imagen importada aquí
+      
+  },
+  {
+      title: "Plan Avanzado",
+      precio: "30",
       data: {
-        uno: "Daños a cosas (Segun Ley)",
-        dos: "Daños a Personas (Segun Ley)",
-        tres: "Exceso de Limite (500 / 800)",
-        cuatro: "Defensa Penal (500 / 800)",
-        cinco: "Muerte Accideltal (Dependiendo de la cantidad de puestos)",
-        seis: "Invalidez Permanente (Dependiendo de la cantidad de puestos)",
-        siete: "Gastos Medicos (Dependiendo de la cantidad de puestos)",
+          uno: "Daños a bienes (Según Ley)",
+          dos: "Daños a personas (Según Ley)",
+          tres: "Cobertura adicional (2.000,00)",
+          cuatro: "Defensa legal (2.000,00)",
+          cinco: "Cobertura por muerte accidental (Variable según ocupantes)",
+          seis: "Cobertura por invalidez permanente (Variable según ocupantes)",
+          siete: "Gastos médicos (Variable según ocupantes)",
       },
-      asegurado:{
-        uno: "Segun Ley",
-        dos: "Segun Ley",
-        tres: "500 / 800",
-        cuatro: "500 / 800",
-        cinco: "Dependiendo de la cantidad de puestos",
-        seis: "Dependiendo de la cantidad de puestos",
-        siete: "Dependiendo de la cantidad de puestos",
-      }
-    },
-    {
-      title: "Plan Responsable",
-      precio: "30", // Asigna la imagen importada aquí
-      data: {
-        uno: "Daños a cosas (Segun Ley)",
-        dos: "Daños a Personas (Segun Ley)",
-        tres: "Exceso de Limite (2.000,00)",
-        cuatro: "Defensa Penal (2.000,00)",
-        cinco: "Muerte Accideltal (Dependiendo de la cantidad de puestos)",
-        seis: "Invalidez Permanente (Dependiendo de la cantidad de puestos)",
-        siete: "Gastos Medicos (Dependiendo de la cantidad de puestos)",
-      },
-      asegurado:{
-        uno: "Segun Ley",
-        dos: "Segun Ley",
-        tres: "2.000,00",
-        cuatro: "2.000,00",
-        cinco: "Dependiendo de la cantidad de puestos",
-        seis: "Dependiendo de la cantidad de puestos",
-        siete: "Dependiendo de la cantidad de puestos",
-      }
-    },
-  ];
+      
+  },
+];
 
-  export default cards;
+export default cards;
