3e283f44d5bd4f8e15ab58db05116c674bf604b2
GabR3924 <garc2004@gmail.com>
2024-07-26 10:44:47 -0400

    data

--- src/data.js
+++ src/data.js
@@ -1,45 +1,70 @@
 const cards = [
   {
-      title: "Plan Básico",
-      precio: "10",
-      data: {
-          uno: "lorem ipsumament aliquet",
-          dos: "lorem ipsumament aliquet",
-          tres: "lorem ipsumament aliquet",
-          cuatro: "lorem ipsumament aliquet",
-          cinco: "lorem ipsumament aliquet",
-          seis: "lorem ipsumament aliquet",
-          siete: "lorem ipsumament aliquet",
-      },
 
+    title: "Plan Precavido ",
+    precio: "10", // Asigna la imagen importada aquí
+    data: {
+      uno: "Daños a cosas (Segun Ley)",
+      dos: "Daños a Personas (Segun Ley)",
+      tres: "Exceso de Limite (250,00)",
+      cuatro: "Defensa Penal (250,00)",
+      cinco: "Muerte Accideltal (Dependiendo de la cantidad de puestos)",
+      seis: "Invalidez Permanente (Dependiendo de la cantidad de puestos)",
+      siete: "Gastos Medicos (Dependiendo de la cantidad de puestos)",
+    },
+    asegurado:{
+      uno: "Segun Ley",
+      dos: "Segun Ley",
+      tres: "250,00",
+      cuatro: "250,00",
+      cinco: "Dependiendo de la cantidad de puestos",
+      seis: "Dependiendo de la cantidad de puestos",
+      siete: "Dependiendo de la cantidad de puestos",
+    }
   },
   {
-      title: "Plan Intermedio",
-      precio: "20",
-      data: {
-        uno: "lorem ipsumament aliquet",
-        dos: "lorem ipsumament aliquet",
-        tres: "lorem ipsumament aliquet",
-        cuatro: "lorem ipsumament aliquet",
-        cinco: "lorem ipsumament aliquet",
-        seis: "lorem ipsumament aliquet",
-        siete: "lorem ipsumament aliquet",
+    title: "Plan Prudente ",
+    precio: "20", // Asigna la imagen importada aquí
+    data: {
+      uno: "Daños a cosas (Segun Ley)",
+      dos: "Daños a Personas (Segun Ley)",
+      tres: "Exceso de Limite (500 / 800)",
+      cuatro: "Defensa Penal (500 / 800)",
+      cinco: "Muerte Accideltal (Dependiendo de la cantidad de puestos)",
+      seis: "Invalidez Permanente (Dependiendo de la cantidad de puestos)",
+      siete: "Gastos Medicos (Dependiendo de la cantidad de puestos)",
     },
-      
+    asegurado:{
+      uno: "Segun Ley",
+      dos: "Segun Ley",
+      tres: "500 / 800",
+      cuatro: "500 / 800",
+      cinco: "Dependiendo de la cantidad de puestos",
+      seis: "Dependiendo de la cantidad de puestos",
+      siete: "Dependiendo de la cantidad de puestos",
+    }
   },
   {
-      title: "Plan Avanzado",
-      precio: "30",
-      data: {
-        uno: "lorem ipsumament aliquet",
-        dos: "lorem ipsumament aliquet",
-        tres: "lorem ipsumament aliquet",
-        cuatro: "lorem ipsumament aliquet",
-        cinco: "lorem ipsumament aliquet",
-        seis: "lorem ipsumament aliquet",
-        siete: "lorem ipsumament aliquet",
+    title: "Plan Responsable",
+    precio: "30", // Asigna la imagen importada aquí
+    data: {
+      uno: "Daños a cosas (Segun Ley)",
+      dos: "Daños a Personas (Segun Ley)",
+      tres: "Exceso de Limite (2.000,00)",
+      cuatro: "Defensa Penal (2.000,00)",
+      cinco: "Muerte Accideltal (Dependiendo de la cantidad de puestos)",
+      seis: "Invalidez Permanente (Dependiendo de la cantidad de puestos)",
+      siete: "Gastos Medicos (Dependiendo de la cantidad de puestos)",
     },
-      
+    asegurado:{
+      uno: "Segun Ley",
+      dos: "Segun Ley",
+      tres: "2.000,00",
+      cuatro: "2.000,00",
+      cinco: "Dependiendo de la cantidad de puestos",
+      seis: "Dependiendo de la cantidad de puestos",
+      siete: "Dependiendo de la cantidad de puestos",
+    }
   },
 ];
 
